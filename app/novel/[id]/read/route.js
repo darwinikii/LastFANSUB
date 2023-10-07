830442862f391991da63687c209c525d340fffb0
darwinikii <74075511+darwinikii@users.noreply.github.com>
2023-10-07 19:50:32 +0000

    za

--- app/novel/[id]/read/route.js
+++ app/novel/[id]/read/route.js
@@ -1,8 +1,6 @@
-import fs from 'fs'
-
 export async function GET(req, { params }) {
-    var api = await fetch(req.nextUrl.origin + "/api/novel/" + params.id + "/volumes")
+    var api = await fetch("http://lastfansub.repl.co/api/novel/" + params.id + "/volumes")
     api = (await api.json())
 
-    return Response.redirect(req.nextUrl.origin + "/novel/" + params.id + "/volume/" + api.basicList[0].split("-")[0] + "/chapter/" + api.basicList[0].split("-")[1])
+    return Response.redirect("http://lastfansub.repl.co/novel/" + params.id + "/volume/" + api.basicList[0].split("-")[0] + "/chapter/" + api.basicList[0].split("-")[1])
 }
