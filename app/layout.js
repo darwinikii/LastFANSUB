ae598af5dc4e7608d7ca172469c92714185d6e6a
darwinikii <74075511+darwinikii@users.noreply.github.com>
2023-10-09 19:35:33 +0000

    za

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
         `}
         </Script>
         <Script id="google-smth">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TM8MK24C');`}</Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3609867447779558" crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM8MK24C" height="0" width="0" className='hidden'></iframe></noscript>
