1a158d11f96e71e39c454bb9142fe3e4d8ca5a02
darwinikii <Ddevran33@gmail.com>
2023-10-12 10:38:42 +0300

    indexing improvements

---
+++ next-sitemap.config.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+
+module.exports = {
+    siteUrl: 'https://lastfansub.repl.co',
+    generateRobotsTxt: true,
+    exclude: ["/api/*", "/uptime/*", "/api"],
+    additionalPaths: async (config) => {
+      var dataFolder = fs.readdirSync("./data/")
+
+      var list = dataFolder.map(async dataId => {
+        var isData = fs.existsSync("./data/" + dataId + "/data.json")
+        var isVolumes = fs.existsSync("./data/" + dataId + "/volumes/")
+
+        if (isData && isVolumes) return await config.transform(config, "/novel/" + dataId)
+      })
+      
+      list = await Promise.all(list)
+
+      return list
+    },
+  }
