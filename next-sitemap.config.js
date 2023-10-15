5524c5e46598d285ffa0d1985063375560bda619
darwinikii <Ddevran33@gmail.com>
2023-10-15 20:21:05 +0300

    sitemap and indexing fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,14 +5,21 @@ module.exports = {
     siteUrl: 'https://lastfansub.vercel.app',
     generateRobotsTxt: true,
     exclude: ["/api/*", "/uptime/*", "/api"],
-    generateIndexSitemap: false,
+    generateIndexSitemap: true,
+    robotsTxtOptions: {
+      additionalSitemaps: ["https://lastfansub.vercel.app/sitemap-0.xml"]
+    },
     additionalPaths: async (config) => {
       var dataFolder = fs.readdirSync(path.join(process.cwd(), "data", "bin"))
 
       var list = dataFolder.map(async dataId => {
         var isData = fs.existsSync(path.join(process.cwd(), "data", "bin", dataId, "data.json"))
+        
+        if (!isData) return
+        var data = JSON.parse(fs.readFileSync(path.join(process.cwd(), "data", "bin", dataId, "data.json")));
 
-        if (isData) return await config.transform(config, "/novel/" + dataId)
+        console.log(data.type);
+        return await config.transform(config, "/" + data.type.toLowerCase() + "/" + dataId)
       })
       
       list = await Promise.all(list)
