b6f9bead13567d90433c356051b13405894fff63
darwinikii <Ddevran33@gmail.com>
2023-10-17 20:45:56 +0300

    fixx

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import "@pwabuilder/pwainstall";
 import useSWR from 'swr';
 import dynamic from 'next/dynamic'
 
@@ -31,26 +30,6 @@ export default function Page() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 overflow-x-hidden">
-      <div className="flex lg:hidden items-center">
-        <div className="flex items-center mr-36">
-          <Image
-            src="/favicon.png"
-            alt="Logo"
-            desktopSize={[64, 64]}
-          />
-          LastFANSUB
-        </div>
-        <pwa-install
-          className="mr-10 rounded-xl"
-          explainer="Bu uygulama telefonuna yüklenebilir. Bu siteye hızlı erişmeni ve rahat kullanmanı sağlar. Normal yüklenmiş uygulama gibi gözükür fakat cihazınızda yer kaplamaz cihazınıza zarar veremez."
-          featuresheader="Özellikler"
-          descriptionheader="Açıklama"
-          installbuttontext="Yükle"
-          cancelbuttontext="İptal et"
-          iosinstallinfotext="Paylaş düğmesine ve ardından 'Ana Ekrana Ekle'ye dokunun"
-        />
-      </div>
-      
       <Nav className='left-0 top-0 z-10 w-full items-center justify-between font-mono text-sm lg:flex'/>
 
       <div className="flex flex-col items-center max-w-full">
